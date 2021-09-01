ef4a160908cf851c9c55d0705313cc4ab7b7f253
eminemhc42 <89907726+eminemhc42@users.noreply.github.com>
2021-09-01 15:39:14 +0530

    updated zuco changes

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ class OpenCVVideoProcessor(VideoProcessorBase):
 
         def recv(self, frame: av.VideoFrame) -> av.VideoFrame:
             img = frame.to_ndarray(format="bgr24")
-            img = cv2.putText(img, 'OpenCV', (50, 50), cv2.FONT_HERSHEY_SIMPLEX,1, (255, 0, 0), 2, cv2.LINE_AA)            
+            img = cv2.putText(img, 'Zuco Sample', (50, 50), cv2.FONT_HERSHEY_SIMPLEX,1, (255, 0, 0), 2, cv2.LINE_AA)            
             return av.VideoFrame.from_ndarray(img, format="bgr24")
 
 webrtc_ctx = webrtc_streamer(key="sample",desired_playing_state = True,mode=WebRtcMode.SENDRECV,rtc_configuration=RTC_CONFIGURATION,video_processor_factory=OpenCVVideoProcessor,async_processing=True,)
